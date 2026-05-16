65165583c4c1d3a7f911a29b97ecdefcad9c9565
EtienneHoven <etienne.hoven@ibm.com>
2026-05-16 12:30:17 +0200

    add scheduler

--- src/main/java/com/test/test_spring_boot_app/TestSpringBootAppApplication.java
+++ src/main/java/com/test/test_spring_boot_app/TestSpringBootAppApplication.java
@@ -1,13 +1,23 @@
 package com.test.test_spring_boot_app;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
+@Slf4j
 @SpringBootApplication
+@EnableScheduling
 public class TestSpringBootAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestSpringBootAppApplication.class, args);
+	}
+
+	@Scheduled(fixedRate = 3000L)
+	public void testSchedule () {
+		log.info("test123");
 	}
 
 }

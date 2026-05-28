99ccce537c97320efc0b32a3087532a1ddd856df
EtienneHoven <etienne.hoven@ibm.com>
2026-05-28 14:16:21 +0200

    add new sout

--- src/main/java/com/test/test_spring_boot_app/TestSpringBootAppApplication.java
+++ src/main/java/com/test/test_spring_boot_app/TestSpringBootAppApplication.java
@@ -18,7 +18,7 @@ public class TestSpringBootAppApplication {
 	@Scheduled(fixedRate = 3000L)
 	public void testSchedule () {
 		log.info("test123");
-		log.info("test3211111");
+		log.info("test3211111222");
 	}
 
 }
